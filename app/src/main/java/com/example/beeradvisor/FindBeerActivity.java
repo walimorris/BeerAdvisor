b9d381ab0cc0d95fbea74db072c29ff534882c4d
morris <walimmorris@gmail.com>
2020-05-30 02:05:22 -0700

    Initial commit

---
+++ app/src/main/java/com/example/beeradvisor/FindBeerActivity.java
@@ -0,0 +1,22 @@
+package com.example.beeradvisor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class FindBeerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+
+    /**
+     * Method is called when user clicks the "FIND BEER" Button
+     * @param view: The button GUI component is of type View
+     */
+    public void onClickFindBeer(View view) {
+
+    }
